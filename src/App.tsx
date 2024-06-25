e2b6bc6de9dd52a794bc93acefda3acde864e91c
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-06-25 09:07:10 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import 'src/app.css'
 import Routes from 'src/routes'
 
 function App() {
-    return <Routes />
+    return (
+        <div className="app-container">
+            <Routes />
+        </div>
+    )
 }
 
 export default App
