9a57807f3eddce287619c8e28f9a7b9afe66912a
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-06-26 18:43:25 -0300

    feat: add function in root saga

--- src/redux/sagas/root-saga.ts
+++ src/redux/sagas/root-saga.ts
@@ -1,6 +1,8 @@
 import { fork } from 'redux-saga/effects'
 import { watchCreateTask } from './create-task/watch-create-task'
+import { watchCreateUser } from './create-user/watch-create-user'
 
 export function* rootSaga() {
     yield fork(watchCreateTask)
+    yield fork(watchCreateUser)
 }
