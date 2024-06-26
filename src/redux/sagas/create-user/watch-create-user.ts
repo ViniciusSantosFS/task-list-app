a7dfb72afd626709ac9887685dba20c1203145b1
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-06-26 18:15:18 -0300

    feat: add create user logic

--- src/redux/sagas/create-user/watch-create-user.ts
+++ src/redux/sagas/create-user/watch-create-user.ts
@@ -1,6 +1,7 @@
-import { put, takeLatest } from 'redux-saga/effects'
+import { put, select, takeLatest } from 'redux-saga/effects'
 import { CreateUser } from 'src/dto/create-user'
 import { CreateUserActionTypes } from './action-types'
+import { InitialState } from 'src/redux/types'
 
 interface CreateUserAction {
     type: string
@@ -9,10 +10,28 @@ interface CreateUserAction {
 
 export function* createUser({ payload }: CreateUserAction) {
     try {
-        // Cadastrar um usuário com nome e email no estado
+        const users: CreateUser[] = yield select(
+            (state: InitialState) => state.users
+        )
+
+        const isEmailAlreadyRegistered = users.some(
+            (user) => user.email === payload.email
+        )
+
+        if (isEmailAlreadyRegistered) {
+            yield put({
+                type: CreateUserActionTypes.CREATE_USER_FAILURE,
+                payload: {},
+            })
+            return
+        }
+
         yield put({ type: CreateUserActionTypes.CREATE_USER_SUCCESS, payload })
     } catch (error) {
-        console.log('ERROR', error)
+        yield put({
+            type: CreateUserActionTypes.CREATE_USER_FAILURE,
+            payload: { error: 'DEU ERRO' },
+        })
     }
 }
 
