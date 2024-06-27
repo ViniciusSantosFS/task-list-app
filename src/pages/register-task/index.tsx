fa446ba6776f2e3502bed8e6f1d2988569039fef
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-06-27 11:09:40 -0300

    feat: improve register task screen

--- src/pages/register-task/index.tsx
+++ src/pages/register-task/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import RegisterTaskHeader from 'src/components/register-task-header'
+import { CreateTaskActionTypes } from 'src/redux/sagas/create-task/action-types'
 import TaskForm from 'src/components/task-form'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { CreateTask } from 'src/dto/create-task'
 
 function RegisterTask() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const onSubmit = (data: CreateTask) => {
+        const task = new CreateTask(
+            data.title,
+            data.type,
+            data.owner,
+            data.description,
+            data.beginDate,
+            data.endDate,
+            data.deliveryDate
+        )
+
+        dispatch({
+            type: CreateTaskActionTypes.CREATE_TASK,
+            payload: task,
+            navigate,
+        })
+    }
     return (
         <>
-            <TaskForm />
+            <RegisterTaskHeader />
+            <TaskForm onSubmit={onSubmit} />
         </>
     )
 }
